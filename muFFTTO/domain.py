c296a7023ced30476accf3a6a4cde98301a4b5b6
MartinLadecky <m.ladecky@gmail.com>
2023-05-25 15:46:45 +0200

    WIP: adding coordinates for quad and nodal points

--- muFFTTO/domain.py
+++ muFFTTO/domain.py
@@ -7,7 +7,7 @@ class PeriodicUnitCell:
         self.name = name
         self.domain_dimension = domain_size.__len__()  # dimension of problem
         self.domain_size = np.asarray(domain_size, dtype=float)  # physical dimension of domain 1,2, or 3 Dim
-
+        # TODO[Martin] left bottom corner of domain is in [0,0,0] should we change it?
         if not problem_type in ['conductivity', 'elasticity']:
             raise ValueError(
                 'Unrecognised physical problem type {}. Choose from ' \
@@ -24,13 +24,12 @@ class PeriodicUnitCell:
             self.unknown_shape = np.array([self.domain_dimension], dtype=int)
             self.gradient_shape = np.array([self.domain_dimension, self.domain_dimension],
                                            dtype=int)  # mat. data matrix a vector of d components
-
             self.material_data_shape = np.array(
                 [self.domain_dimension, self.domain_dimension, self.domain_dimension, self.domain_dimension],
                 dtype=int)  # mat. data matrix a vector of d components
 
 
-class DiscretizationLibrary:
+class Discretization:
     # Discretization is a container that store all important information about discretization of unit cell
     # such as physical dimension, number of pixels/voxels, FE type, number of quadrature points,
     # number of nodal points, etc....
@@ -42,7 +41,7 @@ class DiscretizationLibrary:
         self.domain_size = cell.domain_size
 
         # number of pixels/voxels, without periodic nodes
-        self.number_of_pixels = np.asarray(number_of_pixels, dtype=int)
+        self.nb_of_pixels = np.asarray(number_of_pixels, dtype=np.intp)
 
         if not discretization_type in ['finite_element', 'finite_difference', 'Fourier']:
             raise ValueError(
@@ -51,9 +50,10 @@ class DiscretizationLibrary:
         self.discretization_type = discretization_type  # could be finite difference, Fourier or finite elements
 
         # pixel properties
-        self.pixel_size = self.domain_size / self.number_of_pixels
+        self.pixel_size = self.domain_size / self.nb_of_pixels
         self.nb_element_per_pixel = None
         self.nb_nodes_per_pixel = None
+        self.nodal_points_coordinates = None
 
         if discretization_type == 'finite_element':
             # finite element properties
@@ -61,6 +61,46 @@ class DiscretizationLibrary:
             self.quadrature_weights = None
             self.quad_points_coord = None
             self.get_discretization_info(element_type)
+            self.unknown_size = [*self.cell.unknown_shape, self.nb_nodes_per_pixel, *self.nb_of_pixels]
+            self.gradient_size = [*self.cell.gradient_shape, self.nb_quad_points_per_element,
+                                  self.nb_element_per_pixel, *self.nb_of_pixels]
+            self.material_data_size = [*self.cell.material_data_shape, self.nb_quad_points_per_element,
+                                       self.nb_element_per_pixel, *self.nb_of_pixels]
+
+    def get_nodal_points_coordinates(self):
+        # TODO[more then one nodal point] add coords for more than one nodal point
+        nodal_points_coordinates = np.zeros([self.domain_dimension, self.nb_nodes_per_pixel, *self.nb_of_pixels])
+        nodal_points_coordinates[:, 0] = np.meshgrid(
+            *[np.arange(0, self.domain_size[d], self.pixel_size[d]) for d in range(0, self.domain_dimension)],
+            indexing='ij')
+
+        return nodal_points_coordinates
+
+    def get_quad_points_coordinates(self):
+        quad_points_coordinates = np.zeros([self.domain_dimension, self.nb_quad_points_per_element,
+                                            self.nb_element_per_pixel, *self.nb_of_pixels])
+
+        for e in range(0, self.nb_element_per_pixel):
+            for q in range(0, self.nb_quad_points_per_element):
+                print(e,q)
+                quad_points_coordinates[:, q, e] = np.meshgrid(
+                    *[np.arange(0 + self.quad_points_coord[d, q, e], self.domain_size[d], self.pixel_size[d]) for d in
+                      range(0, self.domain_dimension)],
+                    indexing='ij')
+
+        return quad_points_coordinates
+
+    def get_unknown_size_field(self):
+        # return zero field with the shape of unknown
+        return np.zeros(self.unknown_size)
+
+    def get_gradient_size_field(self):
+        # return zero field for  the  (discretized)  gradient of temperature/displacement
+        return np.zeros(self.gradient_size)
+
+    def get_material_data_size_field(self):
+        # return zero field for the (discretized) material data
+        return np.zeros(self.material_data_size)
 
     def get_discretization_info(self, element_type):
 
@@ -73,15 +113,15 @@ class DiscretizationLibrary:
                     raise ValueError('Element_type {} is implemented only in 2D'.format(element_type))
                 """ Geometry for 2 linear triangular elements in pixel
                     x_4_______________x_3
-                      |  \            |
-                      |     \  e = 2  |
-                      |        \      |
-                      | e = 1    \    |
+                      |  \     e = 2  |
+                      |     \  q = 1  |
+                      | e = 1  \      |
+                      | q = 1    \    |
                     x_1_______________x_2
                 """
                 self.nb_quad_points_per_element = 1
                 self.nb_element_per_pixel = 2
-                self.nb_nodes_per_pixel = 4
+                self.nb_nodes_per_pixel = 1  # left bottom corner belong to pixel.
                 """  Structure of B matrix: 
                      B(:,:,q,e) --> is B matrix evaluate gradient at point q in  element e
                      B(:,:,q,e) has size [dim,nb_of_nodes/basis_functions] 
@@ -89,10 +129,15 @@ class DiscretizationLibrary:
                      B(:,:,q,e) = [ ∂φ_1/∂x_1  ∂φ_2/∂x_1  ∂φ_3/∂x_1 ∂φ_4/∂x_1 ;
                                     ∂φ_1/∂x_2  ∂φ_2/∂x_2  ∂φ_3/∂x_2 ∂φ_4/∂x_2]   at (q)
                 """
-                self.B_gradient = np.zeros([self.domain_dimension, self.nb_nodes_per_pixel,
+                self.B_gradient = np.zeros([self.domain_dimension, 4,
                                             self.nb_quad_points_per_element, self.nb_element_per_pixel])
                 h_x = self.pixel_size[0]
                 h_y = self.pixel_size[1]
+
+                self.quad_points_coord = np.zeros(
+                    [self.domain_dimension, self.nb_quad_points_per_element, self.nb_element_per_pixel])
+                self.quad_points_coord[:, 0, 0] = [h_x / 3, h_y / 3]
+                self.quad_points_coord[:, 0, 1] = [h_x * 2 / 3, h_y * 2 / 3]
 
                 # @formatter:off
                 self.B_gradient[:, :, 0, 0] = [[-1 / h_x,   1 / h_x,    0,          0],
@@ -135,7 +180,7 @@ class DiscretizationLibrary:
                 """
                 self.nb_quad_points_per_element = 4
                 self.nb_element_per_pixel = 1
-                self.nb_nodes_per_pixel = 4
+                self.nb_nodes_per_pixel = 1  # x1 is the only pixel assigned node, x2 belongs to pixel +1
 
                 #  pixel sizes for better readability
                 h_x = self.pixel_size[0]
@@ -145,13 +190,15 @@ class DiscretizationLibrary:
                 coord_helper[0] = -1. / (np.sqrt(3))
                 coord_helper[1] = +1. / (np.sqrt(3))
 
-                self.quad_points_coord = np.zeros([self.nb_quad_points_per_element, self.domain_dimension])
-                self.quad_points_coord[0, :] = [coord_helper[0], coord_helper[0]]
-                self.quad_points_coord[1, :] = [coord_helper[1], coord_helper[0]]
-                self.quad_points_coord[2, :] = [coord_helper[1], coord_helper[1]]
-                self.quad_points_coord[3, :] = [coord_helper[0], coord_helper[1]]
+                self.quad_points_coord = np.zeros(
+                    [self.domain_dimension, self.nb_quad_points_per_element, self.nb_element_per_pixel])
 
-                self.B_gradient = np.zeros([self.domain_dimension, self.nb_nodes_per_pixel,
+                self.quad_points_coord[:, 0, 0] = [coord_helper[0], coord_helper[0]]
+                self.quad_points_coord[:, 1, 0] = [coord_helper[1], coord_helper[0]]
+                self.quad_points_coord[:, 2, 0] = [coord_helper[1], coord_helper[1]]
+                self.quad_points_coord[:, 3, 0] = [coord_helper[0], coord_helper[1]]
+
+                self.B_gradient = np.zeros([self.domain_dimension, 4,
                                             self.nb_quad_points_per_element, self.nb_element_per_pixel])
                 # Jacobian matrix of transformation from iso-element to current size
 
@@ -160,8 +207,8 @@ class DiscretizationLibrary:
                 inv_jacobian = np.array([[h_y / 2, 0], [0, h_x / 2]]) / det_jacobian
 
                 # construction of B matrix
-                for qp in range(0, self.nb_quad_points_per_element ):
-                    x_q = self.quad_points_coord[qp, :]
+                for qp in range(0, self.nb_quad_points_per_element):
+                    x_q = self.quad_points_coord[:, qp, 0]
                     xi = x_q[0]
                     eta = x_q[1]
                     # @formatter:off
@@ -176,6 +223,14 @@ class DiscretizationLibrary:
                 self.quadrature_weights[1, 0] = h_x * h_y / 4
                 self.quadrature_weights[2, 0] = h_x * h_y / 4
                 self.quadrature_weights[3, 0] = h_x * h_y / 4
+                # we have to recompute positions base on the size of current pixel !!!
+                self.quad_points_coord = np.zeros(
+                    [self.domain_dimension, self.nb_quad_points_per_element, self.nb_element_per_pixel])
+
+                self.quad_points_coord[:, 0, 0] = [h_x/ 2 +h_x*coord_helper[0] / 2, h_y/ 2+  h_y*coord_helper[0]/ 2]
+                self.quad_points_coord[:, 1, 0] = [h_x/ 2 +h_x*coord_helper[1] / 2, h_y/ 2+  h_y*coord_helper[0]/ 2]
+                self.quad_points_coord[:, 2, 0] = [h_x/ 2 +h_x*coord_helper[1] / 2, h_y/ 2+  h_y*coord_helper[1]/ 2]
+                self.quad_points_coord[:, 3, 0] = [h_x/ 2 +h_x*coord_helper[0] / 2, h_y/ 2+  h_y*coord_helper[1]/ 2]
 
             case _:
                 raise ValueError('Element type {} is not implemented yet'.format(element_type))
--- test/test_discretization.py
+++ test/test_discretization.py
@@ -1,9 +1,14 @@
 import unittest
 
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+
 from muFFTTO import domain
 
 
-class MyTestCase(unittest.TestCase):
+class DiscretizationTestCase(unittest.TestCase):
     def test_discretization_initialization(self):
         domain_size = [3, 4]
         problem_type = 'elasticity'
@@ -15,13 +20,101 @@ class MyTestCase(unittest.TestCase):
         discretization_type = 'finite_element'
         element_type = 'bilinear_rectangle'  # 'linear_triangles'
 
-        discretization = domain.DiscretizationLibrary(cell=my_cell,
-                                                      number_of_pixels=number_of_pixels,
-                                                      discretization_type=discretization_type,
-                                                      element_type=element_type)
+        discretization = domain.Discretization(cell=my_cell,
+                                               number_of_pixels=number_of_pixels,
+                                               discretization_type=discretization_type,
+                                               element_type=element_type)
 
-        self.assertEqual(True, False)  # add assertion here
+        self.assertTrue(hasattr(discretization, "cell"), 'Discretization has no "cell" ')
+        self.assertTrue(hasattr(discretization, "domain_dimension"), 'Discretization has no "domain_dimension"')
+        self.assertTrue(hasattr(discretization, "B_gradient"), 'Discretization has no "B_gradient" matrix')
 
+        self.assertTrue(hasattr(discretization, "quadrature_weights"), 'Discretization has no "quadrature_weights" ')
+
+        self.assertTrue(hasattr(discretization, "nb_quad_points_per_element"),
+                        'Discretization has no "nb_quad_points_per_element" ')
+
+        self.assertTrue(hasattr(discretization, "nb_element_per_pixel"), 'Discretization has no "nb_element_per_pixel"')
+        self.assertTrue(hasattr(discretization, "nb_nodes_per_pixel"), 'Discretization has no "nb_nodes_per_pixel" ')
+
+    def test_plot_2D_mesh_lin_triangles(self):
+        domain_size = [3, 4]
+        problem_type = 'conductivity'
+        my_cell = domain.PeriodicUnitCell(domain_size=domain_size,
+                                          problem_type=problem_type)
+
+        number_of_pixels = (4, 5)
+
+        discretization_type = 'finite_element'
+        element_type = 'linear_triangles'  # 'linear_triangles'
+
+        discretization = domain.Discretization(cell=my_cell,
+                                               number_of_pixels=number_of_pixels,
+                                               discretization_type=discretization_type,
+                                               element_type=element_type)
+
+        nodal_coordinates= discretization.get_nodal_points_coordinates()
+        quad_coordinates= discretization.get_quad_points_coordinates()
+        plt.scatter(nodal_coordinates[0,0],nodal_coordinates[1,0])
+        segs1 = np.stack((nodal_coordinates[0,0],nodal_coordinates[1,0]), axis=2)
+        segs2 = segs1.transpose(1, 0, 2)
+        plt.gca().add_collection(LineCollection(segs1))
+        plt.gca().add_collection(LineCollection(segs2))
+        plt.scatter(quad_coordinates[0, 0, 0], quad_coordinates[1, 0, 0])
+        plt.scatter(quad_coordinates[0, 0, 1], quad_coordinates[1, 0, 1])
+
+        plt.show()
+        u_fun_sinx = lambda x: np.sin(x)
+
+        temperature = discretization.get_unknown_size_field()
+        temperature = u_fun_sinx(nodal_coordinates[0,0,:,:])
+#        Du = apply_differential_operator(u, test_mesh_info.B, test_mesh_info);
+
+
+
+
+        temperature_gradient = discretization.get_gradient_size_field()
+
+        conductivity_data = discretization.get_material_data_size_field()
+    def test_plot_2D_mesh_bilin_rectangles(self):
+        domain_size = [3, 4]
+        problem_type = 'conductivity'
+        my_cell = domain.PeriodicUnitCell(domain_size=domain_size,
+                                          problem_type=problem_type)
+
+        number_of_pixels = (4, 5)
+
+        discretization_type = 'finite_element'
+        element_type = 'bilinear_rectangle'  # 'linear_triangles'
+
+        discretization = domain.Discretization(cell=my_cell,
+                                               number_of_pixels=number_of_pixels,
+                                               discretization_type=discretization_type,
+                                               element_type=element_type)
+
+        nodal_coordinates= discretization.get_nodal_points_coordinates()
+        quad_coordinates= discretization.get_quad_points_coordinates()
+        plt.scatter(nodal_coordinates[0,0],nodal_coordinates[1,0])
+        segs1 = np.stack((nodal_coordinates[0,0],nodal_coordinates[1,0]), axis=2)
+        segs2 = segs1.transpose(1, 0, 2)
+        plt.gca().add_collection(LineCollection(segs1))
+        plt.gca().add_collection(LineCollection(segs2))
+        for q in range(0,discretization.nb_quad_points_per_element):
+            plt.scatter(quad_coordinates[0, q, 0], quad_coordinates[1, q, 0])
+
+        plt.show()
+        u_fun_sinx = lambda x: np.sin(x)
+
+        temperature = discretization.get_unknown_size_field()
+        temperature = u_fun_sinx(nodal_coordinates[0,0,:,:])
+#        Du = apply_differential_operator(u, test_mesh_info.B, test_mesh_info);
+
+
+
+
+        temperature_gradient = discretization.get_gradient_size_field()
+
+        conductivity_data = discretization.get_material_data_size_field()
 
 if __name__ == '__main__':
     unittest.main()
