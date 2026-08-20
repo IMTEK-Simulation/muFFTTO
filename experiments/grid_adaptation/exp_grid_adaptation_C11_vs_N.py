fixing bug in gradient

experiments/grid_adaptation/exp_grid_adaptation_C11_vs_N.py
@@ -86,9 +86,7 @@ print("=== end sanity check ===")
 
 # Same input image you used in the original script.
 INPUT_FILE = Path(
-    r"C:\Users\Test\Desktop\JiaLing\HiWi\Simulation"
-    r"\Grain Boundaries Data"
-    r"\Green_Jacobi_eta_0.01_w_10.0_p_0.0_final.npy"
+    "Green_Jacobi_eta_0.01_w_10.0_p_0.0_final.npy"
 )
 
 PROBLEM_TYPE = "conductivity"
@@ -385,9 +383,9 @@ def run_one_N(N: int, communicator) -> dict:
                 macro_gradient_field_ijqxyz=macro_gradient_field,
             )
 
-            macro_gradient_field.s[...] = np.einsum(
-                "ij...,jk...->ik...", macro_gradient_field.s[...], inv_F
-            )
+            # macro_gradient_field.s[...] = np.einsum(
+            #     "ij...,jk...->ik...", macro_gradient_field.s[...], inv_F
+            # )
             discretization.fft.communicate_ghosts(field=macro_gradient_field)
 
             rhs_field.sg.fill(0.0)

experiments/grid_adaptation/exp_grid_adaptation_arbitrary_conductivity_transformed.py
@@ -34,14 +34,14 @@ from muFFTTO.grid_adaptation_arbitrary import (
 )
 from muFFTTO import domain
 from muFFTTO.visualization_utils import plot_field_on_grid
-from muFFTTO.check_homogenization_health import run_homogenization_health_check
+# from muFFTTO.check_homogenization_health import run_homogenization_health_check
 
 # ============================================================================
 # User settings
 # ============================================================================
 
 INPUT_FILE = Path(
-    "/home/martin/Programming/microTopOpt/muFFTTO/experiments/grid_adaptation/uv.lock"
+    '/home/martin/Programming/microTopOpt/muFFTTO/experiments/grid_adaptation/Green_Jacobi_eta_0.01_w_10.0_p_0.0_final.npy'
 )
 
 PROBLEM_TYPE = "conductivity"
@@ -444,11 +444,11 @@ def main() -> None:
             macro_gradient_field_ijqxyz=macro_gradient_field,
         )
 
-        macro_gradient_field.s[...] = np.einsum(
-            "ij...,jk...->ik...",
-            macro_gradient_field.s[...],
-            inv_F,
-        )
+        # macro_gradient_field.s[...] = np.einsum(
+        #     "ij...,jk...->ik...",
+        #     macro_gradient_field.s[...],
+        #     inv_F,
+        # )
         discretization.fft.communicate_ghosts(
             field=macro_gradient_field
         )
@@ -551,7 +551,7 @@ def main() -> None:
         print("det(F) max:", det_F_from_plot.max())
         print("det(F) interior min:", det_F_from_plot[1:-1, 1:-1].min())
         print("det(F) interior max:", det_F_from_plot[1:-1, 1:-1].max())
-    health_report = run_homogenization_health_check(det_F, homogenized_A_ij)
+    #health_report = run_homogenization_health_check(det_F, homogenized_A_ij)
 
 if __name__ == "__main__":
     main()